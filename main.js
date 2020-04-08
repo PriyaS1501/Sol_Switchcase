252c8876212765a8026d21df473a0493585837f8
PriyaS1501 <sawantpriyanka323@gmail.com>
2020-04-09 00:05:55 +0530

    switch case

---
+++ main.js
@@ -0,0 +1,30 @@
+let days = 1;
+let message = undefined;
+
+switch (days) {
+    case 1 :
+        message = "sunday";
+        break;
+    case 1 :
+        message = "Monday" ;
+        break;
+    case 3 :
+        message = "Tuesday" ;
+        break;
+    case 4 :
+        message = "wednesday" ;
+        break;
+    case 5 :
+        message = "thursday" ;
+        break;    
+    case 5 :
+        message = "friday" ;
+        break;    
+    case 5 :
+        message = "saturday" ;
+        break;    
+    default :
+        message = "enter days between 1 to 7";            
+}
+
+console.log(message);
